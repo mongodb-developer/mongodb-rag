189b1ed0da6b2c22f59b91a590dec7388b7eecf5
Michael Lynn <merlynn@gmail.com>
2025-02-11 15:33:15 -0500

    export chunker properly

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import MongoRAG from './core/MongoRAG.js';
 import BatchProcessor from './core/BatchProcessor.js';
 import CacheManager from './core/CacheManager.js';
-import IndexManager from './core/IndexManager.js';  
+import IndexManager from './core/IndexManager.js';
+import Chunker from './core/chunker.js'; 
 
-// Export the main class as both default and named export
 export { MongoRAG as default };
-export { MongoRAG, BatchProcessor, CacheManager };
+export { MongoRAG, BatchProcessor, CacheManager, Chunker };
